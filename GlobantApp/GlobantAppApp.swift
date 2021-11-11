2045626fbd3bf74aaf863e0ce9dd4fe6a8fe521d
Jorge Leal <Jorge.Leal.-ND@disney.com>
2021-11-11 11:19:50 -0500

    Init repo
    Init repo

---
+++ GlobantApp/GlobantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobantAppApp.swift
+//  GlobantApp
+//
+//  Created by Jorge Andres Leal Bernal on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GlobantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
